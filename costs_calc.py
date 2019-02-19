dbf5e094cae2a582156d8c784c70e59681b49f29
Lyzw57 <askuczynski57@gmail.com>
2019-02-19 21:55:15 +0100

    added function responsible for workflow of program

--- costs_calc.py
+++ costs_calc.py
@@ -21,10 +21,24 @@ def calc_non_costs(net_amount, vat_amount, deduction_factor):
     return non_costs
 
 def display_results(costs, vat_amount, non_costs):
-    print("KUP: {0}\n50% VAT: {1}\nNKUP: {2}".format(costs, vat_amount, non_costs))
+    print("KUP: {0}\n50% VAT: {1}\nNKUP: {2}".format(round(costs, 2), round(vat_amount, 2), round(non_costs, 2)))
 
-# def init_calc():
-#     user_input = input("Czy chcesz obliczyć NKUP od wydatku? (t/n) : ")
+def init_calc():
+    user_input = input("Czy chcesz obliczyć NKUP od wydatku? (t/n) : ")
 
-#     while user_input.lower() != 'n':
+    while user_input.lower() != 'n':
+        net_amount = get_net_amount()
+        half_of_vat = get_half_of_vat(get_vat_amount(net_amount, VAT_RATE))
+        non_costs = calc_non_costs(net_amount, half_of_vat, NON_COSTS_PROPORTION)
+        costs = (net_amount + half_of_vat) - non_costs
+        display_results(costs, half_of_vat, non_costs)
 
+        while True:
+            user_input = input("Czy chcesz przeliczyć ponownie? (t/n) : ")
+            if user_input.lower() == 't' or user_input.lower() == 'n':
+                break
+            else:
+                print("Nie ma takiej opcji.")
+
+init_calc()
+        
