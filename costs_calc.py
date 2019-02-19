50f6959919eb2f7985744e77cdd0aef62709b551
Lyzw57 <askuczynski57@gmail.com>
2019-02-19 20:47:57 +0100

    created function printing results to the terminal

--- costs_calc.py
+++ costs_calc.py
@@ -20,4 +20,11 @@ def calc_non_costs(net_amount, vat_amount, deduction_factor):
     non_costs = (net_amount + vat_amount) * deduction_factor
     return non_costs
 
-    
+def display_results(costs, vat_amount, non_costs):
+    print("KUP: {0}\n50% VAT: {1}\nNKUP: {2}".format(costs, vat_amount, non_costs))
+
+# def init_calc():
+#     user_input = input("Czy chcesz obliczyć NKUP od wydatku? (t/n) : ")
+
+#     while user_input.lower() != 'n':
+
